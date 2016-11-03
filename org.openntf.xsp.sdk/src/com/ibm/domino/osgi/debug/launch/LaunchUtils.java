63f9a787d44d4ebaf0c0e76a83df5bcfa278f0c7
Serdar Basegmez <serdar.basegmez@gmail.com>
2016-11-03 16:53:31 +0300

    Add conversion methods for local/remote/junction paths

--- org.openntf.xsp.sdk/src/com/ibm/domino/osgi/debug/launch/LaunchUtils.java
+++ org.openntf.xsp.sdk/src/com/ibm/domino/osgi/debug/launch/LaunchUtils.java
@@ -11,17 +11,25 @@ import java.util.Properties;
 import java.util.Set;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.pde.launching.IPDELauncherConstants;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
+import org.openntf.xsp.sdk.Activator;
 import org.openntf.xsp.sdk.exceptions.AbortException;
 import org.openntf.xsp.sdk.platform.INotesDominoPlatform;
+import org.openntf.xsp.sdk.preferences.XspPreferences;
+import org.openntf.xsp.sdk.utils.CommonUtils;
 import org.openntf.xsp.sdk.utils.StringUtil;
 
 public class LaunchUtils {
 
+	private static final ILog logger = Activator.getDefault().getLog();
+	
+	
 	public static String getBundleSuffix(String id) {
 		if ("org.eclipse.equinox.common".equals(id)) {
 			return "@2:start";
@@ -99,29 +107,30 @@ public class LaunchUtils {
 		return pdeLaunchIniFile.getAbsolutePath();
 	}
 
-	// XXX We receive the remote path and convert it to local 
-	public static String toLocalBundleUrl(String remoteUrl, INotesDominoPlatform ndPlatform) {
-		return remoteUrl;
-	}
-	
-	// XXX We receive a path local to Domino, will convert it to a path remote to Domino
-	public static String toRemotePath(String localPath, INotesDominoPlatform ndPlatform) {
-		return localPath;
-	}
-	
 	public static String getConfigDir(AbstractDominoLaunchConfiguration dominoLaunch, ILaunchConfiguration configuration) {
-		// XXX Modify configuration area for Remote
-		// The config dir is probably on the eclipse machine
-		return dominoLaunch.getConfigDir(configuration).getAbsolutePath().replace('\\', '/');
+		String configDir = fixPathSeparators(dominoLaunch.getConfigDir(configuration).getAbsolutePath()); 
+		
+		String result = toJunctionPath(configDir, dominoLaunch.getNotesDominoPlatform());
+		
+		if(CommonUtils.isEmpty(result)) {
+			throw new RuntimeException("Unable to convert the configuration directory into a local representation (" + configDir + ")");
+		}
+		
+		return result;
 	}
 
 	public static Collection<String> populateBundleList(String osgiBundles, INotesDominoPlatform ndPlatform) {
 		Set<String> bundles = new LinkedHashSet<String>();
 		
 		for(String osgiBundle: osgiBundles.split(",")) {
-			// XXX Convert project bundles to the local format. They are in 'reference:file:...' format
-			// incoming bundles are probably on Eclipse machine.
-			bundles.add(osgiBundle);
+			String localPath = toJunctionPath(osgiBundle.substring("reference:file:".length()), ndPlatform);
+			
+			if(CommonUtils.isEmpty(localPath)) {
+				String message = MessageFormat.format("Unable to convert the bundle \"{0}\" to a local representation. Check your settings.", osgiBundle);
+				logger.log(new Status(Status.WARNING, Activator.PLUGIN_ID, message));
+			} else {
+				bundles.add("reference:file:"+localPath);
+			}
 		}
 		
 		return bundles;
@@ -133,14 +142,14 @@ public class LaunchUtils {
 	 */
 	public static Collection<String> findLinkedRepos(File linksDir) {
 		Collection<String> linkedRepos = new LinkedHashSet<String>();
-
+	
 		if (linksDir.exists() && linksDir.isDirectory()) {
 			File[] links = linksDir.listFiles();
 			
 			if (links != null) {
 				for (File link : links) {
 					FileReader reader = null;
-
+	
 					try {
 						reader = new FileReader(link);
 						Properties linkProps = new Properties();
@@ -162,8 +171,94 @@ public class LaunchUtils {
 				}
 			}
 		}
-
+	
 		return linkedRepos;
+	}
+
+	public static String fixPathSeparators(String path) {
+		if(CommonUtils.isEmpty(path)) {
+			return "";
+		}
+		
+		return path.replace('\\', '/');
+	}
+
+	public static String toLocalPath(String remotePath, INotesDominoPlatform ndPlatform) {
+		// We trust Eclipse classes on that incoming url should be well formed in terms of path separators.
+		if(ndPlatform.isLocal()) {
+			// no change needed.
+			return remotePath;
+		}
+		
+		String localInstall = fixPathSeparators(ndPlatform.getLocalInstallFolder());
+		String localData = fixPathSeparators(ndPlatform.getLocalDataFolder());
+		
+		// Is it under install directory?
+		if(CommonUtils.startsWithIgnoreCase(remotePath, ndPlatform.getRemoteInstallFolder())) {
+			return localInstall + remotePath.substring(ndPlatform.getRemoteInstallFolder().length());
+		}
+		
+		// Is it under data directory?
+		if(CommonUtils.startsWithIgnoreCase(remotePath, ndPlatform.getRemoteDataFolder())) {
+			return localData + remotePath.substring(ndPlatform.getRemoteDataFolder().length());
+		}
+		
+		// If not, we can't support any conversion
+		return null;
+	}
+	
+	public static String toRemotePath(String localPath, INotesDominoPlatform ndPlatform) {
+		String result = fixPathSeparators(localPath);
+		
+		if (ndPlatform.isLocal()) {
+			// no change needed
+			return result;
+		}
+		
+		String localInstall = fixPathSeparators(ndPlatform.getLocalInstallFolder());
+		String localData = fixPathSeparators(ndPlatform.getLocalDataFolder());
+		
+		// Is it under install directory?
+		if(CommonUtils.startsWithIgnoreCase(result, localInstall)) {
+			return ndPlatform.getRemoteInstallFolder() + localPath.substring(localInstall.length());
+		}
+		
+		// Is it under data directory?
+		if(CommonUtils.startsWithIgnoreCase(result, localData)) {
+			return ndPlatform.getRemoteDataFolder() + localPath.substring(localData.length());
+		}
+		
+		// If not, we can't support any conversion
+		return null;
+	}
+	
+	public static String toJunctionPath(String remotePath, INotesDominoPlatform ndPlatform) {
+		// We trust Eclipse classes on that incoming url should be well formed in terms of path separators.
+		if(ndPlatform.isLocal()) {
+			// no change needed.
+			return remotePath;
+		}
+		
+		/** 
+		 * The naming might be confusing here. Here is the explanation:
+		 * 
+		 * The local junction is local to the Eclipse machine. For example, it's your home folder (/Users/HomerSimpson)
+		 * The remote junction is how the remote machine access it. For example, suppose you have mapped your home folder as (Z:\) in the Domino machine.
+		 * 
+		 * The magic will work only if your project files and Eclipse files are UNDER your junction point.
+		 * 	
+		 */
+		
+		String localJunction = XspPreferences.getPreferenceString(XspPreferences.LOCAL_JUNCTION);
+		String remoteJunction = fixPathSeparators(XspPreferences.getPreferenceString(XspPreferences.REMOTE_JUNCTION));
+		
+		// Is it under the Junction path?
+		if(CommonUtils.startsWithIgnoreCase(remotePath, localJunction)) {
+			return StringUtil.prunePath(remoteJunction) + remotePath.substring(localJunction.length());
+		}
+		
+		// If not, we can't support any conversion
+		return null;
 	}
 
 	public static void displayMessage(final boolean isError, final String title, String message, Object... args) {
@@ -182,6 +277,5 @@ public class LaunchUtils {
 		});
 	
 	}
-	
-	
+
 }
