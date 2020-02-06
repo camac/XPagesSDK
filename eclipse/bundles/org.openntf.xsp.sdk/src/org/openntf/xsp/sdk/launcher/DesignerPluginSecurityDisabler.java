315ef23f7d9e490d7fbe6fdeb9535cac78532897
Cameron Gregor <camerongregor@gmail.com>
2020-02-06 15:00:27 +1100

    Merge master into rcplaunch branch

---
+++ eclipse/bundles/org.openntf.xsp.sdk/src/org/openntf/xsp/sdk/launcher/DesignerPluginSecurityDisabler.java
@@ -0,0 +1,23 @@
+package org.openntf.xsp.sdk.launcher;
+
+public class DesignerPluginSecurityDisabler {
+
+	private String platformFile = null;
+	
+	public void removePluginSecurity() {
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+
+		
+		DesignerPluginSecurityDisabler disabler = new DesignerPluginSecurityDisabler();
+		
+		disabler.platformFile = "D:\\Projects\\XPagesSDK\\platform.xml";
+		
+
+	}
+
+}
